8a148cc445f21a9532ddecd779e5e9f30e5e58f7
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2023-05-06 14:45:59 +0300

    feat(layouts/default): change layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,50 +1,37 @@
 <script setup lang="ts">
-const currentYear = new Date().getFullYear()
+const leftDrawerOpen = ref(false)
+
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
 </script>
 
 <template>
-  <div class="layout">
-    <header class="header">
-      <Container>
-        <h2>Header</h2>
-        <LocaleSwitch />
-      </Container>
-    </header>
-    <main class="main">
-      <Container>
+  <q-layout view="lHh LpR lFf">
+    <q-header bordered class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          Nuxt 3 Boilerplate
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <q-page class="q-pa-md">
         <slot />
-      </Container>
-    </main>
-    <footer class="footer">
-      <Container>
-        <p>{{ currentYear }}</p>
-      </Container>
-    </footer>
-  </div>
+      </q-page>
+    </q-page-container>
+
+    <q-footer bordered class="bg-grey-8 text-white q-pa-sm">
+      <p class="q-mb-none">
+        2023
+      </p>
+    </q-footer>
+  </q-layout>
 </template>
-
-<style scoped>
-.layout {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-
-.header,
-.main,
-.footer {
-  padding: 12px 0;
-}
-
-.main {
-  flex: 1;
-}
-
-.header {
-  border-bottom: 1px solid gainsboro;
-}
-
-.footer {
-  border-top: 1px solid gainsboro;
-}
-</style>
