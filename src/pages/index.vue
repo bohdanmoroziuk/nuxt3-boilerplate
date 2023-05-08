5f469f797b6e67a9d76e00c09ffa8840776dbc76
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2023-05-08 12:30:08 +0300

    feat(i18n): delete module

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,6 @@
 <script setup lang="ts">
-const { locale } = useI18n()
 </script>
 
 <template>
-  <div>
-    <form>
-      <select v-model="locale">
-        <option value="en">
-          en
-        </option>
-        <option value="fr">
-          fr
-        </option>
-      </select>
-    </form>
-    <h1>{{ $t('welcome') }}</h1>
-  </div>
+  <div />
 </template>
